e137988a3a917379e515626ff2d916b1c1984a47
Saifullaht <mrsaifullah025@gmail.in>
2024-07-10 23:31:34 +0500

    update

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const cardbtn = document.querySelector('#cardbtn');
 const main = document.querySelector("#main");
 
 
+let elementuthanahai = null;
 
- 
- 
+
 const addTask = (event) => {
     event.preventDefault();
     const currentForm = event.target;
@@ -14,7 +14,8 @@ const addTask = (event) => {
     const ticket = createTicket(value);
     if (!value) return; 
 
-
+    parent.insertBefore(ticket, parent.firstElementChild); // Insert before the form
+    
     parent.insertBefore(ticket, currentForm);
     const h3value = parent.children[0].innerText;
     if(!Array.isArray(savedtasks[h3value])){
@@ -29,10 +30,9 @@ const addTask = (event) => {
 }
 
 
-
 const createcard = (cardtitle) => {
     
-
+    
     const mydiv = document.createElement("div")
     const myh3 = document.createElement("h3")
     const myform1 = document.createElement("form")
@@ -44,24 +44,45 @@ const createcard = (cardtitle) => {
     
     myinput.setAttribute("type" , "text");
     myinput.setAttribute("placeholder" , "Add task");
-
+    
     myh3.appendChild(h3text)
     myform1.appendChild(myinput)
     mydiv.appendChild(myh3)
     mydiv.appendChild(myform1)
-
-    myform1.addEventListener("submit", addTask);
-
-    return mydiv;
     
+    myform1.addEventListener("submit", addTask);
+    mydiv.addEventListener("dragstart", (event) => {
+        elementuthanahai = event.target;
+    });
+
+    mydiv.addEventListener("dragleave" , (event ) => event.preventDefault())
+    mydiv.addEventListener("dragover" , (event ) => event.preventDefault())
+    
+    mydiv.addEventListener("drop", (event) => {
+    const woelement = event.target
+    if ( woelement.className.includes("column")) {
+        woelement.appendChild(elementuthanahai);
+      
+    }
+    if ( woelement.className.includes("ticket")) {
+        woelement.parentElement.appendChild(elementuthanahai);
+        const form = event.target.parentElement.querySelector("form");
+        form.insertBefore(elementuthanahai, form.firstElementChild);
+    }
+});
+
+
+return mydiv;
+
     
     
 }
 
+
 // myform1.reset()
+
 cardbtn.addEventListener("click",() => {
     
-
     const cardtitle = prompt ("Enter card title")
     
     const yourdiv = createcard(cardtitle);
@@ -74,12 +95,23 @@ const createTicket = (value) => {
     const ticket = document.createElement("p");
     const elementText = document.createTextNode(value);
     ticket.setAttribute("draggable", "true");
+    ticket.setAttribute("class","ticket");
     ticket.appendChild(elementText);
+    
+    ticket.addEventListener("mousedown", (event) => {
+        dropho = event.target;
+        // console.log("1");
+    });
+    
+
 
     return ticket;
 };
 
 let savedtasks = JSON.parse(localStorage.getItem('savedtasks'));
+
+
+ 
 
 if (!savedtasks){
     savedtasks = {};
@@ -89,11 +121,11 @@ for (const title in savedtasks) {
     const card = createcard(title);
 
     const arrayOfTasks = savedtasks[title];
-
+    
     for (let i = 0; i < arrayOfTasks.length; i++) {
-    const p = createTicket(arrayOfTasks[i]);
+        const p = createTicket(arrayOfTasks[i]);
     card.insertBefore(p, card.lastElementChild);
     
-    }
+}
     main.insertBefore(card, cardbtn);
 }
